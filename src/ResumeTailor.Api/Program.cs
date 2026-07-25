cb980da90c7e70547247137023ab700aff23dbe7
Brian Chodur <bchodur@gmail.com>
2026-07-25 18:25:02 -0500

    Add Scalar so OpenAPI has a UI

--- src/ResumeTailor.Api/Program.cs
+++ src/ResumeTailor.Api/Program.cs
@@ -1,11 +1,20 @@
 using ResumeTailor.Infrastructure;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.MapControllers();
 
